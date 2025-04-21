cb549c0ec1f22b289a706dce2a7c9c3c2af19901
parven-dev <ryan.cu1200@gmail.com>
2025-04-21 14:51:01 +0530

    initial commit

---
+++ 950_reveal_cards_in_increasing_order/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: list[int]) -> list[int]:
+        return deck
+    
+    
+    
+
+deck = [17,13,11,2,3,5,7]
+s1 = Solution()
+print(s1.deckRevealedIncreasing(deck))
