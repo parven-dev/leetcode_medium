3eedd89e1529671f180fd63d163767aa7b397279
parven-dev <ryan.cu1200@gmail.com>
2025-04-21 21:37:06 +0530

    initial commit

---
+++ 2685_count_the_number_of_complete_components/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countCompleteComponents(self, n: int, edges: list[list[int]]) -> int:
+        return n, edges
+    
+    
+n = 6
+edges = [[0,1],[0,2],[1,2],[3,4]]
+s1 = Solution()
+print(s1.countCompleteComponents(n, edges))
