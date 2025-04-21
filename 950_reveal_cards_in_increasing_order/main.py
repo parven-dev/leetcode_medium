0ad78a68e0bf2845936624965c1888bb9d821dfb
parven-dev <ryan.cu1200@gmail.com>
2025-04-21 14:59:32 +0530

    using queue FIFO

--- 950_reveal_cards_in_increasing_order/main.py
+++ 950_reveal_cards_in_increasing_order/main.py
@@ -1,9 +1,16 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: list[int]) -> list[int]:
-        return deck
-    
-    
-    
+        reveal_cards = []
+     
+        sorted_nums = sorted(deck)
+        
+        for i in reversed(sorted_nums):
+            if reveal_cards:
+                poped_card = reveal_cards.pop()
+                reveal_cards.insert(0, poped_card)
+            reveal_cards.insert(0, i)
+                
+        return reveal_cards
 
 deck = [17,13,11,2,3,5,7]
 s1 = Solution()
