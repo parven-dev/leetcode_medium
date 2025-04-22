b3cd06928382743ea03e6021b5f754ca1622ed54
parven-dev <ryan.cu1200@gmail.com>
2025-04-22 21:24:41 +0530

    DFS postorder

--- 2265_count_nodes_equal_to_average_of_subtree/main.py
+++ 2265_count_nodes_equal_to_average_of_subtree/main.py
@@ -11,11 +11,37 @@ class TreeNode(object):
             if arr[i] is not None:
                 temp = TreeNode(arr[i])
                 root = temp
-                
                 root.left = self.insert_level_order(arr, root.left, 2 * i + 1, n)
                 root.right = self.insert_level_order(arr, root.right, 2 * i + 2, n)
         return root
     
+    def averageOfSubtree(self, root) -> int:
+        self.counter = 0
+     
+        def dfs_helper(node):
+            if not node:
+                return (0, 0)
+            
+            sum_left, lefts = dfs_helper(node.left)
+            sum_right, rights  = dfs_helper(node.right)
+            # print(sum_left, lefts, sum_right, rights)
+            
+            sums = sum_left + sum_right + node.val
+            count = lefts + rights + 1
+            
+            average = sums // count
+            
+            if average == node.val:
+                self.counter +=1
+            return (sums, count)
+        dfs_helper(root)
+        return self.counter
+                
+            
+            
+    
+    
+
   
 
 
@@ -26,9 +52,11 @@ tree = TreeNode(None)
 
 root = tree.insert_level_order(arr, None, 0, arr_len)
 
-print(root.val)
-print(root.left.val)
-print(root.right.val)
+print(tree.averageOfSubtree(root))
+
+# print(root.val)
+# print(root.left.val)
+# print(root.right.val)
 
 
 
