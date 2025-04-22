4b6545ae38882c36c78e8f5906145cac0b4a33fe
parven-dev <ryan.cu1200@gmail.com>
2025-04-22 16:57:16 +0530

    created binary tree

--- 2265_count_nodes_equal_to_average_of_subtree/main.py
+++ 2265_count_nodes_equal_to_average_of_subtree/main.py
@@ -1,17 +1,34 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def averageOfSubtree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+
+class TreeNode(object):
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+        
+    def insert_level_order(self,arr, root, i, n):
+        
+        if i < n :
+            if arr[i] is not None:
+                temp = TreeNode(arr[i])
+                root = temp
+                
+                root.left = self.insert_level_order(arr, root.left, 2 * i + 1, n)
+                root.right = self.insert_level_order(arr, root.right, 2 * i + 2, n)
         return root
     
-s1 = Solution()
-root = [4,8,5,0,1,None,6]
-print(s1.averageOfSubtree(root))
+  
+
+
+arr = [4,8,5,0,1,None,6]
+arr_len = len(arr)
+
+tree = TreeNode(None)
+
+root = tree.insert_level_order(arr, None, 0, arr_len)
+
+print(root.val)
+print(root.left.val)
+print(root.right.val)
+
+
+
