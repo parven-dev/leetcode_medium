b1986d2443da75500491c174b22fc2f8959047a1
parven-dev <ryan.cu1200@gmail.com>
2025-04-22 16:39:20 +0530

    initial commit

---
+++ 2265_count_nodes_equal_to_average_of_subtree/main.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def averageOfSubtree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return root
+    
+s1 = Solution()
+root = [4,8,5,0,1,None,6]
+print(s1.averageOfSubtree(root))
