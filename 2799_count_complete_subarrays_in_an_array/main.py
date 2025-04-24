97ae2bd79cfc69552142aed794b27ea45e8f5ed1
parven-dev <ryan.cu1200@gmail.com>
2025-04-24 16:13:01 +0530

    initial commit

---
+++ 2799_count_complete_subarrays_in_an_array/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countCompleteSubarrays(self, nums: list[int]) -> int:
+        return nums
+    
+    
+    
+    
+    
+nums = [1,3,1,2,2]
+
+s1 = Solution()
+print(s1.countCompleteSubarrays(nums))
