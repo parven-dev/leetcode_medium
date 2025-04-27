d9409d19834f857c724bd8d0990924ca0f1a744f
parven-dev <ryan.cu1200@gmail.com>
2025-04-27 22:08:51 +0530

    added index

--- 2545_sort_the_students_by_their_kth_score/main.py
+++ 2545_sort_the_students_by_their_kth_score/main.py
@@ -1,7 +1,13 @@
 class Solution:
     def sortTheStudents(self, score: list[list[int]], k: int) -> list[list[int]]:
-        return score, k
-    
+        
+        index_value = []
+        for i  in range(len(score)):
+            index_value.append(score[i][k])
+        
+        return index_value
+         
+                
 score = [[10,6,9,1],[7,5,11,2],[4,8,3,15]]
 k = 2  
 s1 = Solution()
