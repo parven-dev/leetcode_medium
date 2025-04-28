35551ace7d0282e3c93f31bb5024a0922a3bcc1d
parven-dev <ryan.cu1200@gmail.com>
2025-04-28 16:13:27 +0530

    initial commit

---
+++ 1302_deepest_leaves_sum/main.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root) -> int:
+        return root
+    
+    
+
+root = [1,2,3,4,5,None,6,7,None,None,None,None,8]
+
+s1 = Solution()
+print(s1.deepestLeavesSum(root))
+
