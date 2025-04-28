4fc2d0d529dc0763158860a8ac7cf20e8e8dc9a7
parven-dev <ryan.cu1200@gmail.com>
2025-04-28 17:09:32 +0530

    BFS,  level order Traversal

--- 1302_deepest_leaves_sum/main.py
+++ 1302_deepest_leaves_sum/main.py
@@ -9,8 +9,38 @@ class TreeNode:
         
 class Solution:
     def deepestLeavesSum(self, root) -> int:
-        return root
     
+        def dfs_level_order_helper(root):
+            
+            if root is None:
+                return 
+            
+            queue = [root]
+            
+            while queue:
+                queue_size = len(queue)
+                curr_level = []
+            
+                for _ in range(queue_size):
+                    poped_node = queue.pop(0)
+                    curr_level.append(poped_node.val)
+                    
+                # print(poped_node.val)
+
+                    if poped_node.left:
+                        queue.append(poped_node.left)
+
+                        # my_queue.append(poped_node.left.val)
+                    if poped_node.right:
+                        queue.append(poped_node.right)
+            return curr_level
+
+                        # my_queue.append(poped_node.right.val)
+                        
+                
+        sum_of_deepest_nodes = dfs_level_order_helper(root)
+        return sum(sum_of_deepest_nodes)
+        
     
 
 # root = [1,2,3,4,5,None,6,7,None,None,None,None,8]
@@ -30,4 +60,6 @@ root.right.right.right = TreeNode(8)
 
 s1 = Solution()
 # print(s1.deepestLeavesSum(root))
-print(root.right.right.right.val)
+# print(root.right.right.right.val)
+# print(s1.dfs_level_order(root))
+print(s1.deepestLeavesSum(root))
