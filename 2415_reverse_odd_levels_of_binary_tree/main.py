ef7a47e4d05cbe21b27f74b1846a345a7c9737df
parven-dev <ryan.cu1200@gmail.com>
2025-04-29 16:34:29 +0530

    added TreeNode class

--- 2415_reverse_odd_levels_of_binary_tree/main.py
+++ 2415_reverse_odd_levels_of_binary_tree/main.py
@@ -14,5 +14,17 @@ class Solution:
 
 s1 = Solution()
 
-root = [2,3,5,8,13,21,34]
+# root = [2,3,5,8,13,21,34]
+
+root = TreeNode(2)
+
+root.left = TreeNode(3)
+root.left.left = TreeNode(8)
+root.left.right = TreeNode(13)
+
+root.right = TreeNode(5)
+root.right.left = TreeNode(21)
+root.right.right = TreeNode(34)
+
 print(s1.reverseOddLevels(root))
+
