fea1ea807ae389bcbf4b46c5f9e18f5bafa1e212
parven-dev <ryan.cu1200@gmail.com>
2025-04-29 16:39:11 +0530

    added level order traversal

--- 2415_reverse_odd_levels_of_binary_tree/main.py
+++ 2415_reverse_odd_levels_of_binary_tree/main.py
@@ -10,6 +10,20 @@ class Solution:
     def reverseOddLevels(self, root):
         return root
     
+    def level_order_traversal(self, root):
+        
+        queue = [root]
+        
+        while queue:
+            poped_node = queue.pop(0)
+            print(poped_node.val)
+            if poped_node.left:
+                queue.append(poped_node.left)
+            
+            if poped_node.right:
+                queue.append(poped_node.right)
+                
+    
 
 
 s1 = Solution()
@@ -26,5 +40,6 @@ root.right = TreeNode(5)
 root.right.left = TreeNode(21)
 root.right.right = TreeNode(34)
 
-print(s1.reverseOddLevels(root))
+# print(s1.reverseOddLevels(root))
+print(s1.level_order_traversal(root))
 
