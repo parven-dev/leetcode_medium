1d0fdb2c820ee3b282c62695dbfdfc222cfca676
ryan <39787691+parven-dev@users.noreply.github.com>
2025-06-05 17:24:16 +0530

    Binary Tree

--- 2415_reverse_odd_levels_of_binary_tree/main.py
+++ 2415_reverse_odd_levels_of_binary_tree/main.py
@@ -8,27 +8,41 @@ class TreeNode:
         
 class Solution:
     def reverseOddLevels(self, root):
-        return root
     
-    def level_order_traversal(self, root):
-        
-        queue = [root]
-        
-        while queue:
-            poped_node = queue.pop(0)
-            print(poped_node.val)
-            if poped_node.left:
-                queue.append(poped_node.left)
             
-            if poped_node.right:
-                queue.append(poped_node.right)
+        if root is None:
+                return 
+            
+        queue = [root]
+        level = 0
+            
+        while queue:
+            queue_size = len(queue)
+            curr_level = []
                 
-    
+            for _ in range(queue_size): 
+                poped_node = queue.pop(0)
+                curr_level.append(poped_node)
+                    
+                if poped_node.left:
+                    queue.append(poped_node.left)
+                    
+                if poped_node.right:
+                    queue.append(poped_node.right)
+
+            if level % 2 == 1:
+                values = [node.val for node in curr_level]
+                values.reverse()
+                for i, node in enumerate(curr_level):
+                    node.val = values[i]
+            level += 1 
+            
+        return root.val
+        
 
 
 s1 = Solution()
 
-# root = [2,3,5,8,13,21,34]
 
 root = TreeNode(2)
 
@@ -40,6 +54,7 @@ root.right = TreeNode(5)
 root.right.left = TreeNode(21)
 root.right.right = TreeNode(34)
 
+print(s1.reverseOddLevels(root))
+
 # print(s1.reverseOddLevels(root))
-print(s1.level_order_traversal(root))
 
